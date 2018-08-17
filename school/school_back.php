32842e7f94595cce5eac8019d29c62ae0e478d35
sudopower <nkirandroid@gmail.com>
2018-08-17 17:30:00 +0530

    School Done

--- school/school_back.php
+++ school/school_back.php
@@ -1,54 +1,18 @@
 <?php
-$target_dir = "img/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
-if(isset($_POST["submit"])) {
-    
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
-    } else {
-        echo "File is not an image.";
-        $uploadOk = 0;
-    }
-}
-
-if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
-
-} else {
-     $tmp_name = $_POST['institute_name'].$_POST['phone'].".jpg";
-     
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_dir . $tmp_name)) {
-        
-    } else {
-        echo "Sorry, there was an error uploading your file.";
-    }
-}
 $servername = "localhost";
 $username = "root";
-$password = "";
+$dbpassword = "";
 $dbname = "testune";
 $name = $_POST['institute_name'];
-$qual = $_POST['qual'];
 $phone = $_POST['phone'];
-$phone2 = $_POST['phone2'];
-$exp = $_POST['exp'];
+$prom = $_POST['prom'];
+$desc = $_POST['desc'];
 $email = $_POST['email'];
-$secemail = $_POST['secemail'];
-
-$dob = $_POST['datepicker'];
+$password = $_POST['password'];
 $address = $_POST['address'];
-$country = $_POST['country'];
-
- $result = "";
-
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-$check="SELECT * FROM content_manager WHERE email_id = '$email'";
+$conn = new mysqli($servername, $username, $dbpassword, $dbname);
+$check="SELECT * FROM school WHERE email_id = '$email'";
 $result1 = $conn->query($check);
 $num_rows = mysqli_num_rows($result1);
 
@@ -61,28 +25,31 @@ if ($num_rows>=1) {
 else {
    
    
-    $sql = "INSERT INTO content_manager (name, dob,address,phone_number,email_id,nationality,qualification,experience,photo,phone_no2,sec_email_id,username)
-VALUES ('$name','$dob','$address','$phone','$email','$country','$qual','$exp','$tmp_name','$phone2','$secemail','$email');";
+    $sql = "INSERT INTO school (institute_name, details,promoters,address,phone_no,email_id,username,password)
+VALUES ('$name','$desc','$prom','$address','$phone','$email','$email','$password');";
  $sql .= "SELECT LAST_INSERT_ID()"; 
- 
+
  if ($conn->multi_query($sql)) {
-    do {
+    do { 
         /* store first result set */
         if ($result = $conn->store_result()) {
             while ($row = $result->fetch_row()) {
-                printf("%s\n", $row[0]);
+               
                 $var = (string) $row[0];
             }
-            $cont_id = "inst_".$var."";
-            $sqli = "UPDATE  content_manager SET content_manager_id = '$cont_id' where sno= $var";
+            $inst_id = "inst_".$var."";
+            $sqli = "UPDATE  school SET institute_id = '$inst_id' where sno= $var";
+            echo "Data Saved";
           
             $conn->query($sqli);
             $result->free();
             
         }
-        /* print divider */
        
     } while ($conn->next_result());
+}
+else{
+    echo "Failed";
 }
 
 //$result = $conn->multi_query($sql);
