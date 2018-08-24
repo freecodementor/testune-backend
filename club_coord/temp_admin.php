d1b18827f6e07ba5bb29d4e057756ec974fa89b1
sudopower <nkirandroid@gmail.com>
2018-08-24 19:26:42 +0530

    add course/ebook/test to course

--- club_coord/temp_admin.php
+++
@@ -1,5 +0,0 @@
-<a href="./video/video.php?id=vid_1">Update Video</a>
-<a href="./ebook/ebook.php?id=ebk_64">Update ebook</a>
-<a href="./article/article.php?id=art_3">Update article</a>
-<a href="./course/course.php?id=test_13">Update Course</a>
-<a href="./test/test.php?id=test_13">Update Test</a>
