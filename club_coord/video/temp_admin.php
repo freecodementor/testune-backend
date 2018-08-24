55878f88aeaf25367799d48d08fb861d5d789e43
sudopower <nkirandroid@gmail.com>
2018-08-24 16:13:09 +0530

    Add video to course

---
+++ club_coord/video/temp_admin.php
@@ -0,0 +1,4 @@
+<a href="video.php?id=vid_1">Update Video</a>
+<a href="./ebook/ebook.php?id=ebk_64">Update ebook</a>
+<a href="./article/article.php?id=art_3">Update ebook</a>
+<a href="./online_test/online_test.php?id=test_13">Update Test</a>
